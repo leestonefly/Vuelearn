57e17d009cf109c600b7ab03cf0fc523d6f01784
liyf <787441351@qq.com>
2019-05-09 15:25:13 +0800

    完成按钮基本样式

--- src/pages/a.js
+++
@@ -1,7 +0,0 @@
-console.log('111')
-import '../css/index.css'
-import '../css/text.scss'
-
-const div = document.createElement('div');
-div.innerText='Holle world!';
-document.body.appendChild(div);
---
+++ src/pages/button.js
@@ -0,0 +1,12 @@
+Vue.component('g-button', {
+    template: `<button class="g-button">按钮</button>`
+});
+
+new Vue({
+    el: '#app',
+    data: {
+        message: 'hi'
+    },
+
+});
+
