155d813289804db890a1e02fc6f3126c6db38d7f
liyf <787441351@qq.com>
2019-05-10 15:33:51 +0800

    5.10 下班

--- src/vue/button.vue
+++ src/vue/button.vue
@@ -1,44 +1,70 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-        <svg v-if="icon" class="icon"><use :xlink:href="`#i-${icon}`"></use></svg>
-        <slot></slot>
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
     </button>
 
 </template>
 
 <script>
     export default {
-        props:['icon','iconPosition']
+        props: ['icon', 'iconPosition']
     }
 </script>
 
 <style lang="scss">
-    .g-button{
+    .g-button {
         margin: 20px;
         font-style: var(--font-size);
         height: var(--button-height);
-        padding:0 1em;
+        padding: 0 1em;
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
-        display: inline-flex;justify-content: center;align-content:center ;
-        &:hover{
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
+
+        &:hover {
             border-color: var(--border-color-hover);
         }
-        &:active{
-            background-color:var(--button-active-bg) ;
+
+        &:active {
+            background-color: var(--button-active-bg);
         }
-        &:focus{
+
+        &:focus {
             outline: none;
         }
-        &.icon-right{
-            > .icon{
-                order:2;
+
+        > .icon {
+            order: 1
+        }
+
+        > .content {
+            order: 2
+        }
+
+        &.icon-right {
+            > .icon {
+                order: 2;
             }
+
+            > .content {
+                order: 1;
+            }
+
         }
     }
-    .icon{
-        width: 1em;height: 1em;
+
+    .icon {
+        width: 1em;
+        height: 1em;
 
     }
 </style>
