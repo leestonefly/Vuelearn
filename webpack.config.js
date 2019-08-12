1a3cc13e1acf950b2b766b10084121cdefd07ff2
liyf <787441351@qq.com>
2019-08-12 16:08:47 +0800

    error

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,39 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const globby = require('globby');
+// const globby = require('globby');
 const path = require('path');
-const paths = globby.sync(['./src/pages/*.js']);
-const entry = {};
-const plugins =[];
-paths.map(p=>{
-    const name = path.basename(p).split('.').slice(0,-1)[0];
-    entry[name]=p;
-    plugins.push(
-        new HtmlWebpackPlugin({
-            filename: `${name}.html`,
-            chunks: [name]
-        }),
-    )
-});
+// const paths = globby.sync(['./src/pages/*.js']);
+// const entry = {};
+// const plugins =[];
+// paths.map(p=>{
+//     const name = path.basename(p).split('.').slice(0,-1)[0];
+//     entry[name]=p;
+//     plugins.push(
+//         new HtmlWebpackPlugin({
+//             filename: `${name}.html`,
+//             chunks: [name]
+//         }),
+//     )
+// });
 
 module.exports = {
     mode: 'production',
-    entry: entry,
-    plugins: plugins,
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: "first.bundle.js",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: "./src/index.html"})
+    ],
     module: {
         rules: [
             {
-                test: [/\.css$/,/\.scss$/],
+                test: [/\.css$/,/\.scss$/,/\.vue$/],
                 use: [
                     "style-loader",
                     "css-loader",
-                    "sass-loader"
+                    "sass-loader",
+                    "vue-loader"
                 ]
             }
         ],
